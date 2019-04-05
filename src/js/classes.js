ca4c3b83bd6abc0f448f5aec45d7d0a705717ad9
Ali Turfah <aturfah@umich.edu>
2019-04-05 18:38:17 -0400

    Add Classes & Skills files

---
+++ src/js/classes.js
@@ -0,0 +1,99 @@
+var classes = {
+    "hero": {
+        "name": "Hero",
+        "skills": [],
+        "img_url": "static/eox-img/hero.png"
+    },
+    "survivalist": {
+        "name": "Survivalist",
+        "skills": [],
+        "img_url": "static/eox-img/survivalist.png"
+    },
+    "protector": {
+        "name": "Protector",
+        "skills": [],
+        "img_url": "static/eox-img/protector.png"
+    },
+    "medic": {
+        "name": "Medic",
+        "skills": [],
+        "img_url": "static/eox-img/medic.png"
+    },
+    "ronin": {
+        "name": "Ronin",
+        "skills": [],
+        "img_url": "static/eox-img/ronin.png"
+    },
+    "warmagus": {
+        "name": "War Magus",
+        "skills": [],
+        "img_url": "static/eox-img/warmagus.png"
+    },
+    "gunner": {
+        "name": "Gunner",
+        "skills": [],
+        "img_url": "static/eox-img/gunner.png"
+    },
+    "zodiac": {
+        "name": "Zodiac",
+        "skills": [],
+        "img_url": "static/eox-img/zodiac.png"
+    },
+    "farmer": {
+        "name": "Farmer",
+        "skills": [],
+        "img_url": "static/eox-img/farmer.png"
+    },
+    "ninja": {
+        "name": "Ninja",
+        "skills": [],
+        "img_url": "static/eox-img/ninja.png"
+    },
+    "sovereign": {
+        "name": "Sovereign",
+        "skills": [],
+        "img_url": "static/eox-img/sovereign.png"
+    },
+    "shogun": {
+        "name": "Shogun",
+        "skills": [],
+        "img_url": "static/eox-img/shogun.png"
+    },
+    "nightseeker": {
+        "name": "Nightseeker",
+        "skills": [],
+        "img_url": "static/eox-img/nightseeker.png"
+    },
+    "landschnekht": {
+        "name": "Landschnekht",
+        "skills": [],
+        "img_url": "static/eox-img/landschnekht.png"
+    },
+    "arcanist": {
+        "name": "Arcanist",
+        "skills": [],
+        "img_url": "static/eox-img/arcanist.png"
+    },
+    "imperial": {
+        "name": "Imperial",
+        "skills": [],
+        "img_url": "static/eox-img/imperial.png"
+    },
+    "highlander": {
+        "name": "Highlander",
+        "skills": [],
+        "img_url": "static/eox-img/highlander.png"
+    },
+    "harbinger": {
+        "name": "Harbinger",
+        "skills": [],
+        "img_url": "static/eox-img/harbinger.png"
+    },
+    "pugilist": {
+        "name": "Pugilist",
+        "skills": [],
+        "img_url": "static/eox-img/pugilist.png"
+    }
+};
+
+console.log(classes);
---
+++ src/js/skills.js
@@ -0,0 +1,94 @@
+/*
+Skill Flags for:
+    Ailment: AILMENT_TARGET_TYPE
+    Bind: BIND_TARGET_TYPE
+    Buff: BUFF_TARGET_[STAT/Type]
+    Debuff: DEBUFF_TARGET_STAT
+    Guard: GUARD_TARGET_TYPE
+    Heals: HEAL_TARGET_[HP/Ailment/Binds/FLAGS]
+*/
+
+var skills = {
+    "test": {
+        "id": "test",
+        "lockdown": [
+            "BIND_SINGLE_HEAD",
+            "BIND_SINGLE_ARM",
+            "BIND_SINGLE_LEG",
+            "BIND_AOE_HEAD",
+            "BIND_AOE_ARM",
+            "BIND_AOE_LEG",
+            "BIND_SELF_ALL",
+            "AILMENT_SINGLE_BLIND",
+            "AILMENT_SINGLE_PARALYZE",
+            "AILMENT_SINGLE_PANIC",
+            "AILMENT_SINGLE_CURSE",
+            "AILMENT_SINGLE_PETRIFY",
+            "AILMENT_SINGLE_POISON",
+            "AILMENT_AOE_BLIND",
+            "AILMENT_AOE_PARALYZE",
+            "AILMENT_AOE_PANIC",
+            "AILMENT_AOE_CURSE",
+            "AILMENT_AOE_PETRIFY",
+            "AILMENT_AOE_POISON",
+            "DEBUFF_AOE_AILMENT",
+            "DEBUFF_AOE_BIND",
+            "BUFF_PARTY_AILMENT",
+            "BUFF_PARTY_BIND"
+        ],
+        "healing": [
+            "HEAL_SINGLE_HP",
+            "HEAL_ROW_HP",
+            "HEAL_PARTY_HP",
+            "HEAL_SELF_HP",
+            "BUFF_PARTY_HEAL",
+            "HEAL_SINGLE_AILMENT",
+            "HEAL_ROW_AILMENT",
+            "HEAL_PARTY_AILMENT",
+            "HEAL_SINGLE_BIND",
+            "HEAL_ROW_BIND",
+            "BUFF_ROW_AILMENT",
+            "BUFF_PARTY_AILMENT",
+            "HEAL_SELF_TP",
+            "HEAL_SINGLE_TP",
+            "HEAL_ROW_TP",
+            "HEAL_PARTY_TP"
+        ],
+        "dps_support": [
+            "BUFF_ROW_MATK",
+            "BUFF_ROW_ATTACK",
+            "BUFF_SELF_ATTACK",
+            "BUFF_SINGLE_ATTACK",
+            "BUFF_SELF_SPEED",
+            "BUFF_SELF_ACCURACY",
+            "BUFF_ROW_ACCURACY",
+            "BUFF_AOE_ACCURACY",
+            "DEBUFF_SINGLE_DEFENSE",
+            "DEBUFF_ROW_DEFENSE",
+            "DEBUFF_AOE_DEFENSE",
+            "DEBUFF_SINGLE_EVASION",
+            "DEBUFF_ROW_EVASION",
+            "DEBUFF_AOE_EVASION"
+        ],
+        "dmg_mitigation": [
+            "DEBUFF_SINGLE_ATTACK",
+            "DEBUFF_ROW_ATTACK",
+            "DEBUFF_AOE_ATTACK",
+            "BUFF_SELF_DEFENSE",
+            "BUFF_SINGLE_DEFENSE",
+            "BUFF_ROW_DEFENSE",
+            "BUFF_AOE_DEFENSE",
+            "GUARD_SINGLE_REDIRECT",
+            "GUARD_ROW_REDIRECT",
+            "GUARD_PARTY_REDIRECT",
+            "GUARD_ROW_GUARD",
+            "GUARD_PARTY_PHYSGUARD",
+            "GUARD_PARTY_ELEMGUARD",
+            "BUFF_SINGLE_AGGRO"
+        ],
+        "negatives": [],
+        "damage_type": [],
+    },
+};
+
+console.log(skills)
