898b5820069dc43f583944ddc96c6537ca91edc0
Ali Turfah <aturfah@gmail.com>
2019-04-11 12:10:36 -0400

    Preliminary skill submission

--- src/js/subPanel.js
+++ src/js/subPanel.js
@@ -3,6 +3,25 @@ import ClassDropdown from './classDropdown';
 import '../css/subPanel.css'
 
 class SubPanel extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            "chosenSkills": []
+        }
+    }
+
+    toggleSkill(skillName) {
+        console.log(skillName);
+        newSkills = this.state.chosenSkills;
+        if (!newSkills.includes(skillName)) {
+            newSkills.push(skillName);
+        }
+        this.setState({
+            "chosenSkills": newSkills
+        });
+    }
+
     render() {
         let className = "SubPanel";
         if (this.props.visible === false) {
@@ -14,11 +33,14 @@ class SubPanel extends Component {
         var image_url = this.props.images[this.props.chosen_class + '.png'];
         var classSkills = selectedClass.skills;
 
-        console.log("HERE")
+        console.log(classSkills)
+        var skillMenu = []
+
         return (
             <div className={className}>
                 <ClassDropdown selectable_classes={this.props.selectable_classes} update_method={this.props.update_method} panel_index={this.props.index} active_class={this.props.chosen_class}></ClassDropdown>
                 (I am {selectedClass.name} @ panel {this.props.index})
+                {skillMenu}
                 <img src={image_url} alt={selectedClass.name + " Image"} height='250px'></img>
 
             </div>
