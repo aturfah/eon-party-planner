51801f3bf6ca4cdd9587028313329c9dee24538d
Ali Turfah <aturfah@gmail.com>
2019-04-10 20:05:21 -0400

    Add Actual header content

--- src/js/header.js
+++ src/js/header.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import '../css/header.css';
 
 class Header extends Component {
     render() {
         return (
             <div className="Header">
-                Header Goes Here
+                <h1>Etrian Odyssey Nexus Party Planner</h1>
+                <h5><a href="https://github.com/aturfah/eon-party-planner">Github <i class="fab fa-github"></i></a></h5>
             </div>
         );
     }
