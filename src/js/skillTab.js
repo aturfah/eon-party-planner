2409051a106dd8a40bf7a9ef56c981d328e0973e
Ali Turfah <aturfah@gmail.com>
2019-04-14 15:40:18 -0400

    Docstrings

--- src/js/skillTab.js
+++ src/js/skillTab.js
@@ -1,21 +1,37 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * Little button thing for a skill
+ */
 class SkillTab extends Component {
+  /**
+   * Updates skill when mouse hovers
+   */
   onHoverFunc() {
     const skillInfo = this.props.skillData;
     this.props.hoverCallback(skillInfo['id']);
   }
 
+  /**
+   * Updates skill when mouse leaves
+   */
   onLeaveFunc() {
     this.props.hoverCallback(null);
   }
 
+  /**
+   * Activates skill on click
+   */
   onClickFunc() {
     const skillInfo = this.props.skillData;
     this.props.updateCallback(skillInfo['id']);
   }
 
+  /**
+   * Renders this React class
+   * @return {div} Rendered app
+   */
   render() {
     const skillData = this.props.skillData;
     const callbackFunc = this.onClickFunc.bind(this);
