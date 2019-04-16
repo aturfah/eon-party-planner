79a24b1c77a13fa008af1ba0fbb3f9dd2310ea87
Ali Turfah <aturfah@gmail.com>
2019-04-16 19:52:28 -0400

    move generateTabs to be class function

--- src/js/skillDataTable.js
+++ src/js/skillDataTable.js
@@ -5,27 +5,6 @@ import PropTypes from 'prop-types';
 import '../css/sidebar.css';
 
 /**
- * Creates headers for tabs
- * @param {Array} headers Panel titles
- * @param {Func} clickCallback Callback function on click
- * @return {Array} Array of panel headers
- */
-function createTabs(headers, clickCallback) {
-  const results = [];
-  headers.forEach(function(header, index) {
-    const headerTab = (
-      <Col key={index} className="sidebar-tab-label"
-        onClick={() => clickCallback(index)}>
-        {header}
-      </Col>
-    );
-    results.push(headerTab);
-  });
-
-  return results;
-}
-
-/**
  * Table that renders with relevant skill data
  */
 class SkillDataTable extends Component {
@@ -49,9 +28,36 @@ class SkillDataTable extends Component {
    * @param {Int} index Index in headers of panel
    */
   changeTab(index) {
+    if (index === this.state.activePanel) {
+      return;
+    }
+    console.log("Changing to tab", index);
     const newState = this.state;
     newState.activePanel = index;
     this.setState(newState);
+  }
+
+
+  /**
+   * Creates headers for tabs
+   * @param {Array} headers Panel titles
+   * @param {Func} clickCallback Callback function on click
+   * @return {Array} Array of panel headers
+   */
+  createTabs() {
+    const results = [];
+    const clickCallback = this.changeTab.bind(this);
+    this.headers.forEach(function(header, index) {
+      const headerTab = (
+        <Col key={index} className="sidebar-tab-label"
+          onClick={() => clickCallback(index)}>
+          {header}
+        </Col>
+      );
+      results.push(headerTab);
+    });
+
+    return results;
   }
 
   /**
@@ -59,7 +65,7 @@ class SkillDataTable extends Component {
    * @return {div} Rendered app
    */
   render() {
-    const tabs = createTabs(this.headers, this.changeTab.bind(this));
+    const tabs = this.createTabs();
 
     return (
       <div>
